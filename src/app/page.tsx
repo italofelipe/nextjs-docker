046098ca304b02e956ac6f35cdaa332167d51603
italofelipe <felipe.italo@hotmail.com>
2024-04-27 14:56:00 -0300

    feat: added placeholder api req

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,67 @@
-"use client";
-
-import { useEffect } from "react";
 import axios from "axios";
 
-const Home = () => {
-  useEffect(() => {
-    axios
-      .get("https://brapi.dev/api/quote/PETR4", {
-        headers: {
-          Authorization: `Bearer ${process.env.API_KEY}`,
-        },
-      })
-      .then((res) => console.log("response", res))
-      .catch((err) => console.error("Houve um erro", err));
-  }, []);
+const fetchData = () => {
+  return axios
+    .get<ApiResponse>("https://brapi.dev/api/quote/PETR4", {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error("Houve um erro", err);
+      return null;
+    });
+};
 
-  useEffect(() => {
-    console.log('Env', process.env);
-  }, [process.env]);
+const Home = async () => {
+  const data = await fetchData();
+
+  console.log("Data", data);
   return (
     <div>
-      <h1>Project made with Docker</h1>
+      <h1>Ação: {data?.results[0].longName}</h1>
     </div>
   );
 };
 
 export default Home;
+
+interface ApiResponse {
+  results: Result[];
+  requestedAt: string;
+  took: string;
+}
+
+interface Result {
+  currency: string;
+  twoHundredDayAverage: number;
+  twoHundredDayAverageChange: number;
+  twoHundredDayAverageChangePercent: number;
+  marketCap: number;
+  shortName: string;
+  longName: string;
+  regularMarketChange: number;
+  regularMarketChangePercent: number;
+  regularMarketTime: string;
+  regularMarketPrice: number;
+  regularMarketDayHigh: number;
+  regularMarketDayRange: string;
+  regularMarketDayLow: number;
+  regularMarketVolume: number;
+  regularMarketPreviousClose: number;
+  regularMarketOpen: number;
+  averageDailyVolume3Month: number;
+  averageDailyVolume10Day: number;
+  fiftyTwoWeekLowChange: number;
+  fiftyTwoWeekLowChangePercent: number;
+  fiftyTwoWeekRange: string;
+  fiftyTwoWeekHighChange: number;
+  fiftyTwoWeekHighChangePercent: number;
+  fiftyTwoWeekLow: number;
+  fiftyTwoWeekHigh: number;
+  symbol: string;
+  priceEarnings: number;
+  earningsPerShare: number;
+  logourl: string;
+}
